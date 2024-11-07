6d261fd76c9d9f03dc037eb0e1365326fb562dc3
Jk <juanandrs.montoyagaleano@gmail.com>
2024-11-07 14:30:05 -0500

    Tutorial 6 completo

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for helloworld_project project.
+URL configuration for helloworld project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  # Make sure include is imported
+
+# TUTORIAL 6B
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls")),
+    path('', include('pages.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
